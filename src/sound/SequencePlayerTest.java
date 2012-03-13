14475dc9bbda407d03c152b0a87dea5979a8c2ca
Chase Lambert <theicfire@gmail.com>
2012-03-12 22:07:12 -0400

    everything again

---
+++ src/sound/SequencePlayerTest.java
@@ -0,0 +1,16 @@
+package sound;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SequencePlayerTest {
+	@Test
+	public void testPlaySample1() {
+		
+	}
+	
+	@Test
+	public void testPlaySample2() {
+		
+	}
+}
