d82e0e7a705c31a1f5c7c27e18b6e0ae9a054d60
Will Grathwohl <gwohl@mit.edu>
2012-03-13 21:05:50 -0400

    Started second song

--- src/sound/SequencePlayerTest.java
+++ src/sound/SequencePlayerTest.java
@@ -84,6 +84,75 @@ public class SequencePlayerTest {
 	
 	@Test
 	public void testPlaySample2() {
+		SequencePlayer player;
+        try {
+
+            // create a new player, with 120 beats (i.e. quarter note) per
+            // minute, with 2 tick per quarter note
+            player = new SequencePlayer(200, 12);
+
+            player.addNote(new Pitch('E').toMidiNote(), 0, 6);
+            player.addNote(new Pitch('F').accidentalTranspose(1).toMidiNote(), 0, 6);
+            
+            player.addNote(new Pitch('E').toMidiNote(), 6, 12);
+            player.addNote(new Pitch('F').accidentalTranspose(1).toMidiNote(), 6, 12);
+            
+            player.addNote(new Pitch('E').toMidiNote(), 18, 24);
+            player.addNote(new Pitch('F').accidentalTranspose(1).toMidiNote(), 18, 24);
+            
+            player.addNote(new Pitch('C').toMidiNote(), 30, 36);
+            player.addNote(new Pitch('F').accidentalTranspose(1).toMidiNote(), 30, 36);
+            
+            player.addNote(new Pitch('E').toMidiNote(), 36, 48);
+            player.addNote(new Pitch('F').accidentalTranspose(1).toMidiNote(), 36, 48);
+            
+            player.addNote(new Pitch('G').transpose(Pitch.OCTAVE).toMidiNote(), 48, 60);
+            player.addNote(new Pitch('B').toMidiNote(), 48, 60);
+            player.addNote(new Pitch('G').toMidiNote(), 48, 60);
+            
+//            player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 32*3, 32*4 + 4);
+//            player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 32*3 + 4, 32*3 + 8);
+//            player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 32*3 + 8, 32*3 + 12);
+//            
+//            player.addNote(new Pitch('G').toMidiNote(), 32*3 + 12, 32*3 + 16);
+//            player.addNote(new Pitch('G').toMidiNote(), 32*3 + 16, 32*3 + 20);
+//            player.addNote(new Pitch('G').toMidiNote(), 32*3 + 20, 32*3 + 24);
+//            
+//            player.addNote(new Pitch('E').toMidiNote(), 32*3 + 24, 32*3 + 28);
+//            player.addNote(new Pitch('E').toMidiNote(), 32*3 + 28, 32*3 + 32);
+//            player.addNote(new Pitch('E').toMidiNote(), 32*3 + 32, 32*3 + 36);
+//            
+//            player.addNote(new Pitch('C').toMidiNote(), 32*3 + 36, 32*3 + 40);
+//            player.addNote(new Pitch('C').toMidiNote(), 32*3 + 40, 32*3 + 44);
+//            player.addNote(new Pitch('C').toMidiNote(), 32*3 + 44, 32*3 + 48);
+//            
+//            player.addNote(new Pitch('G').toMidiNote(), 32*3 + 48, 32*3 + 48 + 9);
+//            player.addNote(new Pitch('F').toMidiNote(), 32*3 + 48 + 9, 32*3 + 48 + 9 + 3);
+//            player.addNote(new Pitch('E').toMidiNote(), 32*3 + 60, 32*3 + 69);
+//            player.addNote(new Pitch('D').toMidiNote(), 32*3 + 69, 32*3 + 72);
+//            player.addNote(new Pitch('C').toMidiNote(), 32*3 + 72, 32*3 + 72 + 24);
+
+            System.out.println(player);
+
+            // play!
+            player.play();
+
+            /*
+             * Note: A possible weird behavior of the Java sequencer: Even if the
+             * sequencer has finished playing all of the scheduled notes and is
+             * manually closed, the program may not terminate. This is likely
+             * due to daemon threads that are spawned when the sequencer is
+             * opened but keep on running even after the sequencer is killed. In
+             * this case, you need to explicitly exit the program with
+             * System.exit(0).
+             */
+            // System.exit(0);
+
+        } catch (MidiUnavailableException e) {
+            e.printStackTrace();
+        } catch (InvalidMidiDataException e) {
+            e.printStackTrace();
+        }
 		
 	}
 }
