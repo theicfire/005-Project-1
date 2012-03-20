e7c10598e8922ba1dbc27dae4e5a6742bc404d40
Chase Lambert <theicfire@gmail.com>
2012-03-20 01:22:33 -0400

    more lexer

--- src/lexer/Lexer.java
+++ src/lexer/Lexer.java
@@ -61,19 +61,44 @@ public class Lexer {
 		} else {
 			for (int i = 0; i < line.length(); i++) {
 				char c = line.charAt(i);
+				
 				if (Character.isLetter(c)) {
-					addToTokens(curbuilder);
+					// look at the entire note
+					String pattern = "^([a-zA-z])([,']*)(\\d)?(/(\\d)?)?";
+					Pattern r = Pattern.compile(pattern);
+					String restOfLine = line.substring(i);
+					System.out.println("found letter, looking over " + restOfLine);
+					
+					Matcher m = r.matcher(restOfLine);
+					if (m.find()) {
+						if (m.group(1).length() != 1) {
+							throw new RuntimeException("Wrong note format");
+						}
+						char noteName = m.group(1).charAt(0);
+						String modifiers = m.group(2); // like ''' or something
+						if (m.group(3).length() > 1) {
+							throw new RuntimeException("Wrong numerator format");
+						}
+						int numerator = -1;
+						if (m.group(3).length() >= 1) {
+							numerator = Integer.parseInt(m.group(3));
+						}
+						int denom = -1;
+						if (m.groupCount() > 4 && m.group(5).length() >= 1) {
+							denom = Integer.parseInt(m.group(5));
+						}
+					} else {
+						throw new RuntimeException("bad fraction");
+					}
 					int octave = Character.isLowerCase(c) ? 1 : 0;
 					curbuilder = ABCTokenBuilder.createBuilder()
 							.setLexeme(ABCToken.Lexeme.NOTE)
 							.setNoteName(Character.toUpperCase(c))
 							.setNoteOctave(octave);
-				} else if (c == '\'') {
-					curbuilder = curbuilder.increaseOctave();
-				} else if (c == ',') {
-					curbuilder = curbuilder.decreaseOctave();
-				} else if (Character.isDigit(c)) {
-					curbuilder.setNoteDuration
+
+					addToTokens(curbuilder);
+					
+					i += m.group(0).length() - 1; // -1 because 1 is being added at the end
 				} else if (c == '|') {
 					
 				}
