6b8c381d809c25cc03b783d83e589122493c67c3
Will Grathwohl <gwohl@mit.edu>
2012-03-22 15:40:17 -0400

    finished sheduler test

--- src/player/SchedulerTest.java
+++ src/player/SchedulerTest.java
@@ -29,7 +29,7 @@ public class SchedulerTest {
 		}
 	}
 	@Test
-	public void testSchedule12Note() {
+	public void testSchedule3Note() {
 		TunePrimitive note = TunePrimitive.Note(new Fraction(1,1), new Pitch('C'));
 		TunePrimitive note2 = TunePrimitive.Note(new Fraction(1,2), new Pitch('D'));
 		TunePrimitive note3 = TunePrimitive.Note(new Fraction(1,2), new Pitch('E'));
@@ -63,5 +63,63 @@ public class SchedulerTest {
 			e.printStackTrace();
 		}
 	}
+	@Test
+	public void testScheduleTuple() {
+		TunePrimitive note = TunePrimitive.Note(new Fraction(1,2), new Pitch('C'));
+		TunePrimitive note2 = TunePrimitive.Note(new Fraction(1,2), new Pitch('E'));
+		TunePrimitive note3 = TunePrimitive.Note(new Fraction(1,2), new Pitch('G'));
+		Tuple t = new Tuple(3);
+		t.add(note);
+		t.add(note2);
+		t.add(note3);
+		Scheduler s = new Scheduler(100, 6);
+		t.accept(s);
+		try {
+			s.PLAYER.play();
+		} catch (MidiUnavailableException e) {
+			e.printStackTrace();
+		}		
+	}
+	@Test
+	public void testRepeat() {
+		TunePrimitive note = TunePrimitive.Note(new Fraction(1,1), new Pitch('C'));
+		TunePrimitive note2 = TunePrimitive.Note(new Fraction(1,2), new Pitch('D'));
+		TunePrimitive note3 = TunePrimitive.Note(new Fraction(1,2), new Pitch('E'));
+		TuneSequence t = new TuneSequence();
+		t.add(note);
+		t.add(note2);
+		t.add(note3);
+		TuneRepeatable r = TuneRepeatable.fromSchedulable(t);
+		Scheduler s = new Scheduler(100, 4);
+		r.accept(s);
+		try {
+			s.PLAYER.play();
+		} catch (MidiUnavailableException e) {
+			e.printStackTrace();
+		}		
+	}
+	@Test
+	public void testMultibleEndings() {
+		TunePrimitive note = TunePrimitive.Note(new Fraction(1,1), new Pitch('C'));
+		TunePrimitive note2 = TunePrimitive.Note(new Fraction(1,2), new Pitch('D'));
+		TunePrimitive note3 = TunePrimitive.Note(new Fraction(1,2), new Pitch('E'));
+		TunePrimitive noteE1 = TunePrimitive.Note(new Fraction(1,1), new Pitch('F'));
+		TunePrimitive noteE2 = TunePrimitive.Note(new Fraction(1,1), new Pitch('C'));
+		TuneSequence t = new TuneSequence();
+		t.add(note);
+		t.add(note2);
+		t.add(note3);
+		TuneRepeatable r = TuneRepeatable.fromSchedulable(t);
+		r.add(noteE1);
+		r.add(noteE2);
+		Scheduler s = new Scheduler(100, 4);
+		r.accept(s);
+		try {
+			s.PLAYER.play();
+		} catch (MidiUnavailableException e) {
+			e.printStackTrace();
+		}		
+	}
+	
 
 }
